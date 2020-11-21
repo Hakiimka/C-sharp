82f74f3cf82bcd8126f90735c8949987e39c6a1e
Hakiimka <68197970+Hakiimka@users.noreply.github.com>
2020-11-22 01:47:38 +0700

    Add files via upload

---
+++ MusicParser MVVM/WpfApp8/CommandVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp8
+{
+    class CommandVM : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action<object> Click;
+        public CommandVM(Action<Object> click)
+        {
+            Click = click;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Click(parameter);
+        }
+    }
+}
---
+++ MusicParser MVVM/WpfApp8/DownLoader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp8
+{
+    class DownLoader
+    { 
+        ProgressBar progress;
+        List<string> names;
+        List<string> Urls;
+        int index = 0;
+        WebClient webClient = new WebClient();
+        public DownLoader(ProgressBar pb)
+        {
+            progress = pb;
+        }
+        void pb(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progress.Maximum = e.TotalBytesToReceive;
+            progress.Value = e.BytesReceived;
+        }
+        void EndDownload(object sender, AsyncCompletedEventArgs e)
+        {
+            index++;
+           
+            if(index<names.Count)
+                webClient.DownloadFileAsync(new Uri(Urls[index]), names[index]);
+        }
+        public void Download(string url, string path)
+        {
+            WebClient webClient;
+            webClient = new WebClient();
+            webClient.DownloadFileAsync(new Uri(url), path);
+            webClient.DownloadProgressChanged +=
+                new DownloadProgressChangedEventHandler(pb);
+        }
+        public void DownloadAll(List<string> url, List<string> path)
+        {
+            Urls = url;
+            names = path;
+            index = 0;
+            
+            webClient.DownloadProgressChanged += 
+                new DownloadProgressChangedEventHandler(pb);
+
+            webClient.DownloadFileCompleted +=
+                new AsyncCompletedEventHandler(EndDownload);
+
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(EndDownload);
+        }
+    }
+}
